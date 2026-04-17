3ac8f921cf0ed2e3f4a1a802ad3514d121e38d2f
Laref <223377196+Larefio@users.noreply.github.com>
2026-04-16 20:47:02 -0500

    implementamos ahora si el .js

---
+++ JS-LEVEL4/las-listas-magicas/js/index.js
@@ -0,0 +1,15 @@
+const nombre = 'Luis Arnao';
+const edad = 29;
+const isAlone = true;
+
+const pokemons = ['Pikachu', 'Bulbasur', 'Charmander'];
+console.log(pokemons);
+console.log(pokemons[0]);
+console.log(pokemons[1]);
+console.log(pokemons[2]);
+
+const fruits = ['Apple', 'Banana', 'Cherry'];
+console.log('Banana');
+console.log('fruits'[1]);
+
+
